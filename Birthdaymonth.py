0596ecacd9b88f6e49fa49555b5997f9b4fc7e68
snehakul77 <snehakrishna.kulkarni@outlook.com>
2025-04-24 17:53:34 +0200

    Changes to birdthday calendar project

---
+++ Birthdaymonth.py
@@ -0,0 +1,16 @@
+#WAP to know how many people were born in each months shown. Use Counter from collections
+
+import json
+from collections import Counter
+with open("birthdays.json") as f:
+    birthdays = json.load(f)
+
+num_to_string = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',7: 'July',8:'August',9:'September',10: 'October',11: 'November',12: 'December'}
+
+months = []
+
+for name, birthday_string in birthdays.items():
+    month = int(birthday_string.split("/")[0])
+    months.append(num_to_string[month])
+
+print(Counter(months))
